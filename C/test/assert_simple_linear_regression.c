f1cedaba0263b28d29e270e9e54f294ebd6af33f
Samuel Vishesh Paul <paulsamuelvishesh@live.com>
2024-11-10 06:07:20 +0530

    Simplify assertions in linear regression tests
    Removed redundant string conversions and memory allocations in alpha and beta value checks. Replaced with direct floating point comparisons for improved clarity and efficiency in the test assertions.

--- C/test/assert_simple_linear_regression.c
+++ C/test/assert_simple_linear_regression.c
@@ -13,22 +13,8 @@ void test_1() {
     least_square_fit(x1, y1, 6, &alpha, &beta);
     printf("alpha = %Lf\n", alpha);
     printf("beta = %Lf\n", beta);
-
-    if (!(alpha == 0 || alpha == 1))
-        alpha = alpha * 100 / 100;
-    char *alpha_str = malloc(10 * sizeof(char));
-    snprintf(alpha_str, 10, "%.2Lf", alpha);
-    printf("alpha_str = %s\n", alpha_str);
-    assert(strcmp(alpha_str, "0.00") == 0);
-    free(alpha_str);
-
-    if (!(beta == 0 || beta == 1))
-        beta = (beta * 100) / 100;
-    char *beta_str = malloc(10 * sizeof(char));
-    snprintf(beta_str, 10, "%.2Lf", beta);
-    printf("beta_str = %s\n", beta_str);
-    assert(strcmp(beta_str, "1.00") == 0);
-    free(beta_str);
+    assert(alpha > -0.001f && alpha < 0.001f);
+    assert(beta - 1 > -0.001f && beta - 1 < 0.001f);
 }
 
 void test_2() {
@@ -37,30 +23,18 @@ void test_2() {
     size_t test_sz = 0;
     long double *x1 = range(-100.f, 100.f, 10.f, &test_sz);
     long double *y1 = calloc(test_sz, sizeof(long double));
+    const long double ALPHA = -5.f;
+    const long double BETA = 3.f;
     for (size_t i = 0; i < test_sz; i++) {
-        y1[i] = 3 * x1[i] - 5;
+        y1[i] = BETA * x1[i] + ALPHA;
     }
     least_square_fit(x1, y1, test_sz, &alpha, &beta);
     free(x1);
     free(y1);
     printf("alpha = %Lf\n", alpha);
     printf("beta = %Lf\n", beta);
-
-    if (!(alpha == 0 || alpha == 1))
-        alpha = (long double) ((long int) alpha * 100) / 100.f;
-    char *alpha_str = malloc(10 * sizeof(char));
-    snprintf(alpha_str, 10, "%.2Lf", alpha);
-    printf("alpha_str = %s\n", alpha_str);
-    assert(strcmp(alpha_str, "-5.00") == 0);
-    free(alpha_str);
-
-    if (!(beta == 0 || beta == 1))
-        beta = (long double) ((long int) beta * 100) / 100.f;
-    char *beta_str = malloc(10 * sizeof(char));
-    snprintf(beta_str, 10, "%.2Lf", beta);
-    printf("beta_str = %s\n", beta_str);
-    assert(strcmp(beta_str, "3.00") == 0);
-    free(beta_str);
+    assert(alpha - ALPHA > -0.01 && alpha - ALPHA < 0.01);
+    assert(beta - BETA > -0.01 && beta - BETA < 0.01);
 }
 
 int main() {
